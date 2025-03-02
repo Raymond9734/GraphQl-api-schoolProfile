4101f210b566723b951638179b7a83bd9c999899
Raymond9734 <rymadara97@gmail.com>
2025-03-02 09:05:03 +0300

    Add home handler for serving index.html template

---
+++ Backend/handlers/home.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/Raymond9734/GraphQl-api-schoolProfile/Backend/utils"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("FrontEnd/index.html")
+	if err != nil {
+		utils.LogError("Error parsing template: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	err = temp.Execute(w, nil)
+	if err != nil {
+		utils.LogError("Error executing template: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
